7ed7388a4ecabb0d9016c71de5f981148192f754
Thiem Huynh <thiem.huynh0@gmail.com>
2023-10-12 12:25:05 -0400

    archiving

--- index.js
+++ index.js
@@ -2,4 +2,32 @@
   'use strict';
 
   // YOUR CODE HERE
+
+  // let stopBtn = document.querySelector('#stopButton')
+  // let stopLt = document.querySelector('#stopLight')
+  
+  // let slowBtn = document.querySelector('#slowButton')
+  // let slowLt = document.querySelector('#slowLight')
+
+  // let goBtn = document.querySelector('#goButton')
+  // let goLt = document.querySelector('#goLight')
+
+
+
+
+  const controlBtns = document.querySelectorAll('#controls')[0]
+
+  console.log(controlBtns)
+  
+
+  controlBtns.forEach(button => {
+    button.addEventListener('click', handleButtonClick(e))
+  });
+
+
+
+  function handleButtonClick(e){
+    btnid = e.target.id
+  }
+
 })();
